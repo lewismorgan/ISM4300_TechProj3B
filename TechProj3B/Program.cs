5f2cc0fd442be0a9406c609c6ed62b0a640727a4
Lewis <lewismorgan1995@icloud.com>
2020-09-22 16:29:03 -0400

    Add project files.

---
+++ TechProj3B/Program.cs
@@ -0,0 +1,37 @@
+﻿/// Tech Proj #3b: Array processing
+/// ISM 4300 
+/// University of South Florida
+/// Programmed by Lewis Morgan
+/// 
+using System;
+
+namespace TechProj3B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int[] numbers = new int[25];
+
+            // Populate the array with random numbers (can't use a foreach here because you're assigning elements to the array)
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(0, 1337);
+            }
+
+            // Output the contents of the array
+
+            int index = 0;
+            foreach(int num in numbers)
+            {
+                // It would of been better for me to use a regular 'for' iteration with i, but I wanted to display the use of
+                // both the loops. Doing it this way is a little bit more code since you don't know what the index is with foreach.
+                // Array.IndexOf might work, but it could be wrong if the array has duplicates.
+                Console.WriteLine("Element " + index + "= " + num);
+                index++;
+            }
+        }
+    }
+}
